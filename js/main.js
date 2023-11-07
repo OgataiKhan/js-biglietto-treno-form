f1d8119675231eb41ad96e07428007e1a6976e2e
OgataiKhan <badellino.kevin@gmail.com>
2023-11-07 17:25:35 +0100

    feat: style input section

--- js/main.js
+++ js/main.js
@@ -46,6 +46,8 @@ document.getElementById('submit-btn').addEventListener('click', function(event) 
     event.preventDefault();
 });
 
+
+
 /*
 
     document.getElementById('price').innerHTML = `${finalPrice}€`;
